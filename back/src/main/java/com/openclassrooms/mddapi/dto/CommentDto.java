a78684c390d5551692fd95203e2261c796647611
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-03-04 18:16:41 +0100

    create dtos

---
+++ back/src/main/java/com/openclassrooms/mddapi/dto/CommentDto.java
@@ -0,0 +1,65 @@
+package com.openclassrooms.mddapi.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+public class CommentDto {
+    private Long id;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    private Long postId;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    public CommentDto() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @JsonProperty("post_id")
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    @JsonProperty("created_at")
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @JsonProperty("updated_at")
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
