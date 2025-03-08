7c1dde70e730555706674142cc7cd48880b7c658
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-03-08 18:59:25 +0100

    add user profile feature

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import {PostListComponent} from "./pages/post-list/post-list.component";
 import {SinglePostComponent} from "./pages/single-post/single-post.component";
 import {TopicListComponent} from "./pages/topic-list/topic-list.component";
 import {PostFormComponent} from "./pages/post-form/post-form.component";
+import {UserProfileComponent} from "./pages/user-profile/user-profile.component";
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'account', component: UserProfileComponent },
   { path: 'posts', component: PostListComponent },
   { path: 'post/:id', component: SinglePostComponent },
   { path: 'posts/create', component: PostFormComponent },
