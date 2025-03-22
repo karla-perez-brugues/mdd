198ab6bfcd609d5424ad9eb4b1b98b3b5ba7bfac
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-03-22 12:12:58 +0100

    add validation

--- back/src/main/java/com/openclassrooms/mddapi/controller/CommentController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/CommentController.java
@@ -2,13 +2,12 @@ package com.openclassrooms.mddapi.controller;
 
 import com.openclassrooms.mddapi.dto.CommentDto;
 import com.openclassrooms.mddapi.model.Comment;
-import com.openclassrooms.mddapi.payload.response.MessageResponse;
 import com.openclassrooms.mddapi.service.CommentService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
 
@@ -28,7 +27,7 @@ public class CommentController {
     }
 
     @PostMapping("")
-    public ResponseEntity<CommentDto> create(@PathVariable("id") String postId, @RequestBody CommentDto commentDto, Principal principal) {
+    public ResponseEntity<CommentDto> create(@PathVariable("id") String postId, @Valid @RequestBody CommentDto commentDto, Principal principal) {
         Comment comment = commentService.create(commentDto, Long.valueOf(postId), principal.getName());
         CommentDto commentResponse = commentService.entityToDto(comment);
 
